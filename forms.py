bd96e2f30e16df4ff309f6d0b63ff5a20bb465d5
Nathaniel Reeder <n.ash.reeder@gmail.com>
2023-05-08 00:28:49 -0600

    Struggling with form, not sure why it won't validate. Otherwise should be good.

--- forms.py
+++ forms.py
@@ -1,17 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, SelectField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, BooleanField, SelectField, IntegerField
+from wtforms.validators import DataRequired, Length, InputRequired
 
 class TeamForm(FlaskForm):
-    team_name = StringField('team name', validators=[DataRequired(), Length(min=4, max=255)])
-    submit = SubmitField('submit')
+    team_name = StringField('Team Name', validators=[DataRequired(), Length(min=4, max=255)])
+    submit = SubmitField('Submit')
     
 class ProjectForm(FlaskForm):
     project_name = StringField('Project Name', validators=[DataRequired(), Length(min=4, max=255)])
     description = StringField('Description')
-    completed = BooleanField('Completed', validators=[DataRequired()])
+    completed = BooleanField('Completed')
     team_id = SelectField('Team ID')
     submit = SubmitField('Submit')
     
     def update_teams(self, teams):
         self.team_id.choices = [ (team.id, team.team_name) for team in teams ]
+
+
+        
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from forms import TeamForm, ProjectForm
-from model import User, Team, Project, connect_to_db
+from model import db, User, Team, Project, connect_to_db
 
 app = Flask(__name__)
 
@@ -21,7 +21,10 @@ def add_team():
     team_form = TeamForm()
     
     if team_form.validate_on_submit():
-        print(team_form.team_name.data)
+        team_name = team_form.team_name.data
+        new_team = Team(team_name, user_id)
+        db.session.add(new_team)
+        db.session.commit()
         return redirect(url_for('home'))
     else:
         return redirect(url_for('home'))
@@ -32,10 +35,69 @@ def add_project():
     project_form.update_teams(User.query.get(user_id).teams)
     
     if project_form.validate_on_submit():
-        print(project_form.project_name.data)
+        project_name = project_form.project_name.data
+        project_description = project_form.description.data
+        is_completed = project_form.completed.data
+        team_id = project_form.team_id.data
+        new_project = Project(project_name, team_id, project_description, is_completed)
+        db.session.add(new_project)
+        db.session.commit()
         return redirect(url_for('home'))
     else:
         return redirect(url_for('home'))
+
+@app.route('/user-profile')
+def user_profile():
+    teams = User.query.get(user_id).teams
+    # print(teams)
+    project_list = []
+    for team in teams:
+        projects = Team.query.get(team.id).projects
+        for project in projects:
+            project_list.append(project)
+    # print(project_list)
+    user = User.query.get(user_id).username
+    
+    return render_template('profile.html', teams=teams, project_list=project_list, user=user)
+
+@app.route('/delete-project/<project_id>')
+def delete_project(project_id):
+    
+    if project_id:
+        project_to_delete = Project.query.get(project_id)
+        db.session.delete(project_to_delete)
+        db.session.commit()
+        return redirect(url_for('user_profile'))
+    else:
+        print('Problem With ID')
+        return redirect(url_for('user_profile'))
+    
+@app.route('/update-project/<project_id>')
+def update_project(project_id):
+    project_to_update = Project.query.get(project_id)
+    
+    project_form = ProjectForm()
+    project_form.update_teams(User.query.get(user_id).teams)
+    
+    if project_form.validate_on_submit():
+        print('Valid On Submit')
+        project_name = project_form.project_name.data
+        project_description = project_form.description.data
+        is_completed = project_form.completed.data
+        team_id = project_form.team_id.data
+        
+        project_to_update.name = project_name
+        project_to_update.description = project_description
+        project_to_update.completed = is_completed
+        project_to_update.team_id = team_id
+        
+        db.session.add(project_to_update)
+        db.session.commit()
+        return redirect(url_for('user_profile'))
+    else:
+        print('Not Valid on Submit')
+    
+    return render_template('update.html', project_form=project_form, project_id=project_id)
 
 if __name__ == "__main__":
     connect_to_db(app)
